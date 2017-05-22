edfe4ca1360fc6716268a27348a23fd8d6a9ac69
aehyok <aehyok@vip.qq.com>
2017-05-22 19:09:32 +0800

    添加Microsoft.EntityFrameworkCore和Microsoft.EntityFrameworkCore.SqlServer;

---
+++ aehyok.WebApi/CodeFirstDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace aehyok.WebApi
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CodeFirstDbContext:DbContext
+    {
+    }
+}
