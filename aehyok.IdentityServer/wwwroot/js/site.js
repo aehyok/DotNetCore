ba34ae120b9d2064483d59a6b9b6aeddfbbdf331
aehyok <aehyok@vip.qq.com>
2017-06-09 13:00:20 +0800

    移除aehyok.IdentityServer项目，重新添加

--- aehyok.IdentityServer/wwwroot/js/site.js
+++
@@ -1,7 +0,0 @@
-﻿// Write your Javascript code.
-window.addEventListener("load", function () {
-    var a = document.querySelector("a.PostLogoutRedirectUri");
-    if (a) {
-        window.location = a.href;
-    }
-});
