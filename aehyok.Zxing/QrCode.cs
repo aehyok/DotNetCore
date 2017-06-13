06ff131573d25e3c2cab72d645f1177c476504d4
aehyok <aehyok@vip.qq.com>
2017-06-13 12:32:06 +0800

    添加条码二维码Zxing包，还待实现方法

---
+++ aehyok.Zxing/QrCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace aehyok.Zxing
+{
+    /// <summary>
+    /// ZXing二维码 条形码 生成机制
+    /// </summary>
+    public class QrCode
+    {
+        public static void Create(string text)
+        {
+
+        }
+    }
+}
