780c03b3dfbc653ce6cc2e9f08f5966872703c00
aehyok <aehyok@vip.qq.com>
2017-06-17 17:14:24 +0800

    Zxing引入

--- aehyok.Zxing/QrCode.cs
+++ aehyok.Zxing/QrCode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ZXing;
 using ZXing.Common;
 using ZXing.QrCode;
@@ -12,7 +13,6 @@ namespace aehyok.Zxing
     {
         public static void Create(string text)
         {
-
         }
     }
 }
