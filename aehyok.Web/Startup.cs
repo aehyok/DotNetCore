4c53363863791cc47859d006069ec2309d54aac1
刘启明 <aehyok@vip.qq.com>
2017-08-04 18:25:06 +0800

    bower 引用css 和js

--- aehyok.Web/Startup.cs
+++ aehyok.Web/Startup.cs
@@ -7,11 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace aehyok.Web
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,6 +41,7 @@ namespace aehyok.Web
         {
             loggerFactory.AddConsole();
 
+            env.EnvironmentName = "Test";
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
