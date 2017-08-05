8c538a73828924b38af81eb8d52eb68359cee5fe
aehyok <aehyok@vip.qq.com>
2017-08-05 23:10:00 +0800

    css和js文件引用整理，以及Layout模版的初始化，还待继续处理

---
+++ aehyok.Web/wwwroot/scripts/common.js
@@ -0,0 +1,8 @@
+﻿var Menu = {
+    //点击顶部菜单事件
+    TopMenuItemClick: function (e) {
+        var menuId = e.id;
+        window.location.href = "../Home/Index?menuId="+menuId+"&type=header";
+
+    }
+}
