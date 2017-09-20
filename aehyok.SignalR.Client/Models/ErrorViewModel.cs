8ddb439a8a26bd83cfb1b7a605e717c45de2a1e9
刘启明 <aehyok@vip.qq.com>
2017-09-20 17:01:59 +0800

    添加单独的SignalR 客户端Web

---
+++ aehyok.SignalR.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aehyok.SignalR.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
