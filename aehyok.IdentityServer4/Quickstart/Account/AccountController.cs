a6b2660e004242906e38d1e79f6406efd1ec2942
刘启明 <aehyok@vip.qq.com>
2017-10-10 16:07:55 +0800

    aehyok.IdentityServer4点击登录页面的取消按钮，停留在登录页面

--- aehyok.IdentityServer4/Quickstart/Account/AccountController.cs
+++ aehyok.IdentityServer4/Quickstart/Account/AccountController.cs
@@ -74,7 +74,7 @@ namespace IdentityServer4.Quickstart.UI
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginInputModel model, string button)
         {
-            if (button != "login")
+            if (button != "login")   //取消按钮操作
             {
                 // the user clicked the "cancel" button
                 var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
@@ -84,9 +84,11 @@ namespace IdentityServer4.Quickstart.UI
                     // denied the consent (even if this client does not require consent).
                     // this will send back an access denied OIDC error response to the client.
                     await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
-                    
+
                     // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    return Redirect(model.ReturnUrl);
+
+                    var refererUrl = Request.Headers["Referer"].ToString();
+                    return Redirect(refererUrl);
                 }
                 else
                 {
