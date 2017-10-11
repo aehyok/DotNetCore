cff42538cc948017bc80b6e761388276099369bf
刘启明 <aehyok@vip.qq.com>
2017-10-11 09:48:48 +0800

    设置全局axios Authorization,测试通过博客列表和标签列表

--- aehyok.Web/wwwroot/scripts/oidc_app.js
+++ aehyok.Web/wwwroot/scripts/oidc_app.js
@@ -1,10 +1,12 @@
 ﻿// <reference path="oidc-client.js" />
 
+//从当前url中获取参数值
 function GetUrlRegExp(name) {
     var reg = new RegExp("(^|\\?|&)" + name + "=([^&]*)(\\s|&|$)", "i");
     if (reg.test(window.location.href)) return decodeURIComponent(RegExp.$2.replace(/\+/g, " ")); return "";
 }
 
+//oidc-client 全局配置
 var config = {
     authority: "http://localhost:5000",
     client_id: "js",
@@ -14,7 +16,7 @@ var config = {
     //post_logout_redirect_uri: "http://localhost:5003/Home/Index", //登出后跳转的页面（注释后可以跳转回原页面）
 };
 var mgr = new Oidc.UserManager(config);
-
+var oidcUser = null;
 $(document).ready(function () {
     var menuId = GetUrlRegExp("menuId");
     if (menuId == null || menuId == "") {
@@ -25,12 +27,22 @@ $(document).ready(function () {
     //获取当前用户是否有效，无效需登录
     mgr.getUser().then(function (user) {
         if (user) {
-            //log("User logged in", user.profile);
             $("#UserName").html(user.profile.name);
             console.log("User logged in");
+            oidcUser = user;
+            Vue.prototype.$http = axios;
+
+            //设置全局请求添加Authorization Header
+            axios.interceptors.request.use(
+                config => {
+                    config.headers.Authorization = "Bearer " + user.access_token;
+                    return config;
+                },
+                err => {
+                    return Promise.reject(err);
+                });
         }
         else {
-            //log("User not logged in");
             console.log("User not logged in");
             //用户无效进行跳转IdentityServer4登录
             mgr.signinRedirect({ state: window.location.href });
@@ -38,12 +50,6 @@ $(document).ready(function () {
     });
 });
 
-
-
-function login() {
-    console.info(window.location.href);
-    mgr.signinRedirect({ state: window.location.href }); //登录后跳转到原来页面
-}
 
 function api() {
     mgr.getUser().then(function (user) {
@@ -59,6 +65,7 @@ function api() {
     });
 }
 
+//登出
 function logout() {
     mgr.signoutRedirect();
 }
