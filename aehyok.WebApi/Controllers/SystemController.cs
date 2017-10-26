9742e89a13916ede582bbd3b00934b91972634c8
刘启明 <aehyok@vip.qq.com>
2017-10-26 18:25:40 +0800

    通过CodeFirstDbContext直接在控制器中操作IdentityUserRole实体

--- aehyok.WebApi/Controllers/SystemController.cs
+++ aehyok.WebApi/Controllers/SystemController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using aehyok.Core.Data;
 using aehyok.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using aehyok.Core.Data.Entity;
 
 namespace aehyok.WebApi.Controllers
 {
@@ -20,13 +24,23 @@ namespace aehyok.WebApi.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<ApplicationRole> _roleManager;
 
+        private readonly CodeFirstDbContext _dbContext;
+
+        private readonly IServiceCollection _serviceCollection;
+
         /// <summary>
         /// 构造函数
         /// </summary>
-        public SystemController(UserManager<ApplicationUser> userManager,RoleManager<ApplicationRole> roleManager)
+        public SystemController(UserManager<ApplicationUser> userManager,RoleManager<ApplicationRole> roleManager, CodeFirstDbContext dbContext)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _dbContext = dbContext;
+            
+            this._dbContext = dbContext;
+            //可直接通过CodeFirstDbContext来操作用户角色中间表
+            //var data = _dbContext.UserRoles.Add(new IdentityUserRole<string>() { UserId = "111", RoleId = "111" });
+            //_dbContext.SaveChanges();
         }
 
         /// <summary>
